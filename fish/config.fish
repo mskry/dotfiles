98291442fd259b0e79f1280ad28f559014563598
Mykola Skrypets <mykola.skrypets@hotel-spider.com>
2023-09-26 10:09:07 +0200

    default lazy nvim config, fish completions

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,19 @@
 set PATH $HOME/.cargo/bin $PATH
-set PATH /usr/local/Cellar/llvm/15.0.2/bin $PATH
-set ZELLIJ_CONFIG_DIR $HOME/.config/zellij
+set PATH /usr/local/Cellar/llvm/16.0.6/bin $PATH
 
 # Define alias in shell
 alias vim "nvim"
 alias vi "nvim"
-alias ls "exa --long"
-alias zellij "zellij -l compact"
+alias ls "eza --long"
 
 # Set default editor
 set EDITOR "nvim"
 set VISUAL "nvim"
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    eval (zellij setup --generate-auto-start fish | string collect)
     eval (starship init fish | source)
 end
 
-# pnpm
-set -gx PNPM_HOME "/Users/mac/Library/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
--- nvim/lua/plugins/tabout.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "abecodes/tabout.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "hrsh7th/nvim-cmp",
-  },
-  config = true,
-}
