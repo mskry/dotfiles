1c15df6c75a57982ef130b69c224b037ef7ce69b
Mykola Skrypets <skrypets@gmail.com>
2024-09-18 13:29:22 +0200

    added fish functions and aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,33 +1,41 @@
 alias nv "nvim ."
 
 if type -q eza
-    alias ll "eza -l1 -g --icons"
-    alias lla "ll -a"
+    alias ls="eza"
+end
+
+if type -q bat
+    alias cat="bat"
+end
+
+if type -q fd
+    alias find="fd --type f"
 end
 
 if status is-interactive
     starship init fish | source
 end
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# pnpm
-set -gx PNPM_HOME "/home/ms/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+if type -q mise
+    /home/ms/.local/bin/mise activate fish | source
 end
-# pnpm end
-
 # set default editor for the fzf to open files
 set -gx EDITOR "nvim"
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-
 set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub) (set --names | psub)'
-
-
 # Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
 fzf_configure_bindings
 
-/home/ms/.local/bin/mise activate fish | source
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/ms/miniconda3/bin/conda
+    eval /home/ms/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/ms/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/ms/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/ms/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
---
+++ fish/functions/ll.fish
@@ -0,0 +1,4 @@
+function ll --wraps='eza -l1 -g --icons' --wraps=eza_git --description 'alias ll eza_git'
+  eza_git $argv
+        
+end
