3fde650ee5e2e9fa0e5d5cef316f1d9e3c2f2421
Mykola Skrypets <skrypets@gmail.com>
2025-07-04 23:15:23 +0200

    :ai: copilot as claude models provider

--- nvim/lua/plugins/AI_avante.lua
+++ nvim/lua/plugins/AI_avante.lua
@@ -10,18 +10,23 @@ return {
   opts = {
     -- add any opts here
     -- for example
-    provider = "claude",
+    provider = "copilot",
     providers = {
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-sonnet-4-20250514",
-        timeout = 30000, -- Timeout in milliseconds
-        extra_request_body = {
-          temperature = 0.75,
-          max_tokens = 20480,
-        },
+      copilot = {
+        model = "claude-sonnet-4",
       },
     },
+    -- providers = {
+    --   claude = {
+    --     endpoint = "https://api.anthropic.com",
+    --     model = "claude-sonnet-4-20250514",
+    --     timeout = 30000, -- Timeout in milliseconds
+    --     extra_request_body = {
+    --       temperature = 0.75,
+    --       max_tokens = 20480,
+    --     },
+    --   },
+    -- },
   },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
